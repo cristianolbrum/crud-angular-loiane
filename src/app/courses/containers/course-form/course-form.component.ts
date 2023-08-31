68a85eee60d8e8535a1d19aad120917bea9c31a0
cristiano Brum <cristianolbrum@gmail.com>
2023-08-31 19:44:05 -0400

    aula 34 Validações de Formulário no Front-end

--- src/app/courses/containers/course-form/course-form.component.ts
+++ src/app/courses/containers/course-form/course-form.component.ts
@@ -1,12 +1,13 @@
 import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { NonNullableFormBuilder } from '@angular/forms';
+import { NonNullableFormBuilder, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { ActivatedRoute } from '@angular/router';
 
-import { CoursesService } from '../../services/courses.service';
 import { Course } from '../../model/course';
-import { compileInjectable } from '@angular/compiler';
+import { CoursesService } from '../../services/courses.service';
+import { filter } from 'rxjs';
+
 
 @Component({
   selector: 'app-course-form',
@@ -17,8 +18,12 @@ export class CourseFormComponent implements OnInit {
 
   form = this.formBuilder.group({
     _id: [''],
-    name: [''],
-    category: ['']
+    name: ['', [Validators.required
+      ,Validators.minLength(5)
+      ,Validators.maxLength(100)
+    ]
+    ],
+    category: ['', [Validators.required]]
   });
 
 constructor(private formBuilder: NonNullableFormBuilder,
@@ -53,6 +58,22 @@ ngOnInit(): void {
     name: course.name,
     category: course.category
   })
+}
+
+getErrorMessage(fieldName : string){
+  const field = this.form.get(fieldName);
+  if(field?.hasError('required')){
+    return  'Campo Obrigatório'
+  }
+  if(field?.hasError('minlength')){
+    const requiredLength = field.errors ? field.errors['minlength']['requiredLength'] : 5;
+    return  `Tamanho minimo precisa ser de ${requiredLength} caracteres.` ;
+  }
+  if(field?.hasError('maxlength')){
+    const requiredLength = field.errors ? field.errors['maxlength']['requiredLength'] : 100;
+    return  `Tamanho minimo precisa ser de ${requiredLength} caracteres.` ;
+  }
+  return 'Campo Invalido';
 }
 
 }
